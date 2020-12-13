1de23cd07ef185f519ed4ff3a7fd9fa1ff42e4db
Sapir Malatzky <sapirMal@users.noreply.github.com>
2020-12-13 14:10:55 +0200

    burger css

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -3,12 +3,11 @@ import BuildControl from './BuildControl/BuildControl';
 import classes from './BuildControls.css';
 
 const controls = [
+    {label: 'Meat', type: 'meat'},
     {label: 'Salad', type: 'salad'},
     {label: 'Bacon', type: 'bacon'},
     {label: 'Cheese', type: 'cheese'},
-    {label: 'Meat', type: 'meat'},
-
-];
+    ];
 
 const buildControls = props => {
     return <div className={classes.BuildControls}>
